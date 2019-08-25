1bbbc1d917e2297a4196a36deacbdf28655bd3b1
Davies <davies@iMac-2.local>
2019-08-25 16:06:14 -0600

    count up

--- javascripts/plugins/countupfromdate.js
+++ javascripts/plugins/countupfromdate.js
@@ -4,7 +4,7 @@
  */
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("June 30, 2018 15:00:00", 'countup1'); // ****** Change this line!
+  countUpFromTime("June 30, 2018 17:00:00", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
